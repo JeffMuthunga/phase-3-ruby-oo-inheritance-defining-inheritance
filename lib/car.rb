cf05af83d1b560bfb7b08d22fff8fd22796f6ab4
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-02-15 14:48:45 +0300

    Made changes

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,14 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+require 'pry'
+
+class Car < Vehicle
+    def go
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end
+    def fill_up_tank
+        'filling up!'
+    end
+end
+
+#binding.pry
